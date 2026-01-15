db50cb84575bde985028f780f7359794acc4d1f2
oliviergenevest <olivier.genevest@gmail.com>
2026-01-15 21:45:32 +0100

    fix youtube video bug

--- src/components/video.js
+++ src/components/video.js
@@ -2,19 +2,19 @@ import React from "react";
 
 
 const Video = ({ videoSrcURL, videoTitle, ...props }) => {
-//console.log(videoSrcURL)
-const embedSrc = videoSrcURL.includes('playlist?list') ? videoSrcURL.replace('playlist?list=', '/embed/videoseries?list=') : videoSrcURL.replace('watch?v=', 'embed/')
-  return (
+//console.log(embedSrc)
+const embedSrc = videoSrcURL.includes('playlist?list') ? videoSrcURL.replace('playlist?list=', 'embed/videoseries?list=') : videoSrcURL.replace('watch?v=', 'embed/')
+console.log(embedSrc) ;
+return (
   <>
   <div className="video-responsive">
     <iframe
       src={embedSrc}
       title={videoTitle}
-      type="text/html"
+
       //allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       //frameBorder="0"
-      webkitallowfullscreen="true"
-      mozallowfullscreen="true"
+   
       allowFullScreen
        width="450"
     />
